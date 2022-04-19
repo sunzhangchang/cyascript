9e9593b165e658cb0fc98390162c7ee048750171
sunzhangchang <642311409@qq.com>
2022-04-19 08:23:08 +0800

    ?

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
+#include <regex>
+#include <folly
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
+int main(int argc, char** argv) {
+    const std::string code{R"(println("hello world!"))"};
     return 0;
 }
